7a1bceab9d78e830a4770bae83a8942220c7ed44
Csaba Gyuris <gycsaba@vasvari.hu>
2020-02-24 13:34:09 +0100

    SeaStorage kód munkához

---
+++ TengerekProjekt/validation/SeaStorage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TengerekProjekt.validation
+{
+    class SeaStorage
+    {
+        //Jó tesztesetek: Bering-tenger, Karib-tenger, Aral-tó (beltenger), Nagy-sóstó, Mexikói-öböl, Nagy-Ausztráliai-öböl
+        //Hibát dobó esetek: adriai-tenger, Adriai tenger, Adriai-folyó
+        private string seaName;
+
+        public bool isValid()
+        {
+            if (!isFirstLetterUperCase())
+            {
+                throw 
+            }
+
+            return true;
+        }
+
+
+    }
+}
