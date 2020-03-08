3d98fc50e709baa2e5897db170f9700c250ff1c6
Gyuris Csaba <gycsaba@vasvari.hu>
2020-03-08 18:57:09 +0100

    HousNumberStorage kidolgozásra kész

---
+++ HousNumberStorageProjekt/validation/HousNumberStorage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HousNumberStorageProjekt.validation
+{
+    class HousNumberStorage
+    {
+        //házszám: legfeljebb háromjegyű pozitív szám amit egy pont zár le
+        //hibás házszámok: "", "12","a.", "9999."
+        //jó házszámok: "9.", "12.","123."
+
+
+        private string housnumber;
+
+        public HousNumberStorage(string housnumber)
+        {
+            this.housnumber = housnumber;
+        }
+
+        public bool isValid()
+        {
+            return true;
+        }
+    }
+}
